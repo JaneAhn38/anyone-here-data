38e0292e527b5365d360c26b36f56f4df23735f5
Jane Ahn <jjung9874@gmail.com>
2026-02-19 21:48:05 +0900

    Add files via upload

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,1 +1,42 @@
+-- 01_schema.sql
+-- Anyone Here DB she
 
+CREATE DATABASE IF NOT EXISTS anyoneheredb
+  DEFAULT CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+USE anyoneheredb;
+DROP TABLE IF EXISTS spot_presence;
+DROP TABLE IF EXISTS location_logs;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS spots;
+
+Create Table: CREATE TABLE `spots` (
+  `spot_id` int NOT NULL AUTO_INCREMENT,
+  `spot_name` varchar(20) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `radius_m` int DEFAULT '100',
+  PRIMARY KEY (`spot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+Create Table: CREATE TABLE `users` (
+  `user_id` varchar(30) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+Create Table: CREATE TABLE `location_logs` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(10) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `logged_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `location_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
