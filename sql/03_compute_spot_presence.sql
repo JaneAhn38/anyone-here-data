feecf7ce5a8b5ff918d499cd81e589230a0ff181
Jane Ahn <jjung9874@gmail.com>
2026-02-21 15:30:21 +0900

    Add files via upload

--- sql/03_compute_spot_presence.sql
+++ sql/03_compute_spot_presence.sql
@@ -1,1 +1,18 @@
+-- 03_compute_spot_presence.sql
+USE anyoneheredb;
 
+TRUNCATE TABLE spot_presence;
+
+INSERT INTO spot_presence (spot_id, active_user_count, calculated_at)
+SELECT
+    s.spot_id,
+    COUNT(DISTINCT l.user_id) AS active_user_count,
+    NOW() AS calculated_at
+FROM spots s
+JOIN location_logs l
+  ON ST_Distance_Sphere(
+       POINT(s.longitude, s.latitude),
+       POINT(l.longitude, l.latitude)
+     ) <= s.radius_m
+-- 시간 조건 제거: 샘플 데이터 전체를 기준으로 집계
+GROUP BY s.spot_id;
